95022bc5c8ba0c99094a7c06cba3c7e9c9df9eff
Ady Romantika <adyromantika.ahmad@iproperty.com>
2017-03-29 01:01:14 +0800

    Changes for deployment

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -15,14 +15,18 @@ class Deployment():
         self.elb_client = boto3.client('elb', region_name=region_name)
         self.new_stack = new_stack
         self.new_elb = None
+        self.recordname = None
 
 
-    def get_new_web_elb(self):
+    def get_facts(self):
         response = self.cloudformation_client.describe_stacks(StackName=self.new_stack)
         for output in response['Stacks'][0]['Outputs']:
             if output['OutputKey'] == 'WebElasticLoadBalancer':
                 self.new_elb = output['OutputValue'][7:]
                 print "Our new ELB is " + self.new_elb
+            if output['OutputKey'] == 'RecordName':
+                self.fqdn = output['OutputValue']
+                print "Our FQDN is " + self.recordname
 
 
     def delete_old_stack(self):
@@ -31,7 +35,7 @@ class Deployment():
         )
 
         for stack in response['StackSummaries']:
-            if not re.match('(squarefoot|frontend)-.*', stack['StackName']):
+            if not re.match('frontend-.*', stack['StackName']):
                 print "Skipping stack: " + stack['StackName']
                 continue
             if stack['StackName'] != self.new_stack:
@@ -49,7 +53,7 @@ class Deployment():
                     {
                         'Action': 'UPSERT',
                         'ResourceRecordSet': {
-                            'Name': 'web.dev.sg.squarefoot.ippstatic.com',
+                            'Name': self.recordname,
                             'Type': 'CNAME',
                             'TTL': 60,
                             'ResourceRecords': [
@@ -68,6 +72,7 @@ if not sys.argv[1]:
     exit(1)
 
 DEPLOYER = Deployment(sys.argv[1])
-DEPLOYER.get_new_web_elb()
+DEPLOYER.get_facts()
+# TODO: Check new stack before switching?
 DEPLOYER.updateDns()
 DEPLOYER.delete_old_stack()
