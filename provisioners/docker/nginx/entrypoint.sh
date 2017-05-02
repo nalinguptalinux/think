3516ae6ecf07093128af9d82f76350cd2631274b
Ady Romantika <adyromantika.ahmad@iproperty.com>
2017-05-02 16:42:35 +0800

    Well, let's just remove the headers

--- provisioners/docker/nginx/entrypoint.sh
+++ provisioners/docker/nginx/entrypoint.sh
@@ -8,7 +8,6 @@ then
         /bin/sed -i "s|<HP_INTERNAL_ELB>|$1|g" /etc/nginx/nginx.conf
         /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf
         /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######   For DNS change
-        /bin/sed -i "s|<STACK_NAME>|$4|g" /etc/nginx/nginx.conf
 fi
 
 /usr/sbin/nginx -g 'daemon off;'
