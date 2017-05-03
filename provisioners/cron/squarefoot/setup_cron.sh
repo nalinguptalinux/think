0ff2d7fb89691679348436338701dc9093cb74a6
Harshad Wankhede <harshad.wankhede@rea-group.com>
2017-05-03 16:46:51 +0800

    updating cron scripts

--- provisioners/cron/squarefoot/setup_cron.sh
+++ provisioners/cron/squarefoot/setup_cron.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sed -i 's/mysql_password=password/mysql_password={password_here}/g' /opt/legacy-cron-scripts/mysqldump_apply.sh
+sed -i 's/mysql_password=password/mysql_password=<pass>/g' /opt/legacy-cron-scripts/mysqldump.sh
 
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "0 0 18 1/1 * ? * /opt/legacy-cron-scripts/mysqldump_apply.sh > /var/log/legacy-cron.log && echo date >> /var/log/legacy-cron.log" >> mycron
+echo "0 18 * * * /opt/legacy-cron-scripts/mysqldump.sh > /var/log/legacy-cron.log && echo date >> /var/log/legacy-cron.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
