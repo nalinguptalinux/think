7eaef1fa8dbfa0c36fce1e3f2662992b00c35b29
Harshad Wankhede <harshad.wankhede@rea-group.com>
2017-05-03 13:55:59 +0800

    updating legacy cron cf script

--- provisioners/cron/squarefoot/mysqldump.sh
+++ provisioners/cron/squarefoot/mysqldump.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p /tmp/mysqldump/
-aws s3 cp s3://prod-sg-sfhk-backups/legacy-mysqldump/`date +%F`/backup_data/sf-www.sql.gz /tmp/mysqldump/sf-www.sql.gz --region ${AWS::Region}
+aws s3 cp s3://prod-sg-sfhk-backups/legacy-mysqldump/`date +%F`/backup_data/sf-www.sql.gz /tmp/mysqldump/sf-www.sql.gz --region ap-southeast-1
 zcat /tmp/mysqldump/sf-www.sql.gz | mysql -P '3306' -h 'squarefoot-rds-mysql-b.ciln4pjvy5g8.ap-southeast-1.rds.amazonaws.com' -u 'root' -p '${MySqlPassword}' sf-www
