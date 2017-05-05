d4041b63efb9a75ca13c7adf0756d25ed5ad27fd
Nalin Kant Gupta <nalin.gupta@iproperty.com>
2017-05-05 15:22:13 +0800

    Checking built time
    Checking built time

--- provisioners/docker/nginx/entrypoint.sh
+++ provisioners/docker/nginx/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-# Execute replacements for the internal ELB urls
+# Execute  replacements for the internal ELB urls
 
 if [[ ! -z "$1" ]] && [[ ! -z "$2" ]];
 then
         /bin/sed -i "s|<HP_INTERNAL_ELB>|$1|g" /etc/nginx/nginx.conf
         /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf
-        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######   For DNS change
+        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######   For VPC DNS change
         /bin/sed -i "s|<VERSION>|$4|g" /etc/nginx/nginx.conf
 fi
 
