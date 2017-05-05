49db5e4eb5bfc089ce83f3054c1fb62e54ea7fb8
Harshad Wankhede <harshad.wankhede@rea-group.com>
2017-05-05 18:05:50 +0800

    updtaing rds cf stack , using private subnet

--- provisioners/cron/squarefoot/mysqldump.sh
+++ provisioners/cron/squarefoot/mysqldump.sh
@@ -3,4 +3,4 @@
 export mysql_password=password
 mkdir -p /tmp/mysqldump/
 aws s3 cp s3://prod-sg-sfhk-backups/legacy-mysqldump/`date +%F`/backup_data/sf-www.sql.gz /tmp/mysqldump/sf-www.sql.gz --region ap-southeast-1
-zcat /tmp/mysqldump/sf-www.sql.gz | mysql -P'3306' -h'squarefoot-rds-mysql-b.ciln4pjvy5g8.ap-southeast-1.rds.amazonaws.com' -u'root' -p"$mysql_password" sf-www
+zcat /tmp/mysqldump/sf-www.sql.gz | mysql -P'3306' -h'rds.production.squarefoot-singapore.ipga.local' -u'root' -p"$mysql_password" sf-www
