ba4ba3fe6eb0e5944704adca5d10792206689248
Nalin Kant Gupta <nalin.gupta@iproperty.com>
2017-05-12 17:34:37 +0800

    Time Check

--- provisioners/docker/nginx/entrypoint.sh
+++ provisioners/docker/nginx/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ ! -z "$1" ]] && [[ ! -z "$2" ]];
 then
         /bin/sed -i "s|<HP_INTERNAL_ELB>|$1|g" /etc/nginx/nginx.conf
         /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf
-        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######    For VPC DNS change #
+        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######  For VPC DNS change #
         /bin/sed -i "s|<VERSION>|$4|g" /etc/nginx/nginx.conf
 fi
 
