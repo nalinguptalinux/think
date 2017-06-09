0c337e631dd00b5567dbce2bcd12eccb3cf5db41
Nalin Kant Gupta <nalin.gupta@iproperty.com>
2017-06-09 18:06:29 +0800

    Added 5 variable for New relic

--- provisioners/docker/nginx/entrypoint.sh
+++ provisioners/docker/nginx/entrypoint.sh
@@ -9,6 +9,8 @@ then
         /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf
         /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf      #######  For VPC DNS change #
         /bin/sed -i "s|<VERSION>|$4|g" /etc/nginx/nginx.conf
+        /bin/sed -i "s|New_Relic_Key|$5|g" /etc/nginx-nr-agent/nginx-nr-agent.ini
+        /usr/sbin/service nginx-nr-agent start
 fi
 
 /usr/sbin/nginx -g 'daemon off;'
