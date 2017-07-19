abb7821a708cc06efce951cbe160962872bf9635
Nalin Kant Gupta <nalin.gupta@iproperty.com>
2017-07-19 15:09:52 +0800

    Update Newrelic SSL-error add project for nginx

--- provisioners/docker/nginx/entrypoint.sh
+++ provisioners/docker/nginx/entrypoint.sh
@@ -5,14 +5,15 @@ set -e
 
 if [[ ! -z "$1" ]] && [[ ! -z "$2" ]];
 then
-        /bin/sed -i "s|<HP_INTERNAL_ELB>|$1|g" /etc/nginx/nginx.conf                        #######  HP ELB  change #
-        /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf                    #######  SRP Elb  change #
-        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf                                  #######  For VPC DNS change #
-        /bin/sed -i "s|<VERSION>|$4|g" /etc/nginx-nr-agent/nginx-nr-agent.ini               #######  New-relic-version-append #
-#        /bin/sed -i "s|Front-end|Front-end-$4|g" /etc/nginx/nginx.conf               #######  Header update #
-        /bin/sed -i "s|New_Relic_Key|$5|g" /etc/nginx-nr-agent/nginx-nr-agent.ini           #######  New-relic Key #
-        /bin/sed -i "s|Replace-with-host|Nginx-$6|g" /etc/nginx-nr-agent/nginx-nr-agent.ini #######  New-relic-Hostname #
-        /usr/sbin/service nginx-nr-agent start                                              #######  Service start #
+        /bin/sed -i "s|<HP_INTERNAL_ELB>|$1|g" /etc/nginx/nginx.conf                            #######  HP ELB  change #
+        /bin/sed -i "s|<SRPPDP_INTERNAL_ELB>|$2|g" /etc/nginx/nginx.conf                        #######  SRP Elb  change #
+        /bin/sed -i "s|vpc-dns|$3|g" /etc/nginx/nginx.conf                                      #######  For VPC DNS change #
+        /bin/sed -i "s|<VERSION>|$4|g" /etc/nginx-nr-agent/nginx-nr-agent.ini                   #######  New-relic-version-append #
+        /bin/sed -i "s|Front-end|Front-end-$4|g" /etc/nginx/nginx.conf                          #######  Header update #
+        /bin/sed -i "s|New_Relic_Key|$5|g" /etc/nginx-nr-agent/nginx-nr-agent.ini               #######  New-relic Key #
+        /bin/sed -i "s|Replace-with-host|Nginx-$7-$6|g" /etc/nginx-nr-agent/nginx-nr-agent.ini  #######  New-relic-Hostname-Project-Deployment-ID #
+        /usr/sbin/service nginx-nr-agent start                                                  #######  Service start #
+        /bin/echo $? >> /root/last-status.txt                                                   #######  Last Status #
 fi
 
 /usr/sbin/nginx -g 'daemon off;'
